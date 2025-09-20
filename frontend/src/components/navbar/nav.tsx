c2a1b30dfac736cea06ec194e4fbacf9d94e7aad
OleMarGlo <om.glomsrud@gmail.com>
2025-09-21 00:00:18 +0200

    fix: fix transition of logo and

--- frontend/src/components/navbar/nav.tsx
+++ frontend/src/components/navbar/nav.tsx
@@ -6,12 +6,14 @@ export default function NavBar() {
     return (
         <nav className='bg-yggdrasil-700 fixed w-full'>
             <div className='grid grid-cols-3 justify-between items-center p-4 px-20 fill-yggdrasil-50'>
-                <Logo className='fill-yggdrasil-50 size-14'/>
-                <div className='grid gap-8 grid-flow-col justify-center divide-x divide-yggdrasil-200 border'>
+                <Logo className='fill-foreground size-14 transition-colors duration-300'/>
+                <div className='grid gap-8 grid-flow-col
+                    justify-center divide-x
+                    divide-yggdrasil-200 border'>
                     <Link href='/'>Posts</Link>
                     <Link href='/'>Skills</Link>
                 </div>
-                <div className='justify-self-end'>
+                <div className='justify-self-end h-14'>
                     <ThemeSwitch />
                 </div>
             </div>
