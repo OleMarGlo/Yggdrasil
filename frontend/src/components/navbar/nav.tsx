8a5f1c80e1c47911bc593f95f33dd36c4a20ba2d
OleMarGlo <om.glomsrud@gmail.com>
2025-09-20 19:14:13 +0200

    chore: structure

---
+++ frontend/src/components/navbar/nav.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Logo from '../logo/logo'
+
+export default function NavBar() {
+    return (
+        <nav className='bg-yggdrasil-700 fixed w-full'>
+            <div className='grid grid-cols-3 justify-between items-center p-4 px-20 fill-yggdrasil-50'>
+                <Logo className='fill-yggdrasil-50 size-14'/>
+                <div className='grid gap-8 grid-flow-col justify-center divide-x divide-yggdrasil-200 border'>
+                    <Link href='/'>Posts</Link>
+                    <Link href='/'>Skills</Link>
+                </div>
+                <div className='justify-self-end'>
+                    <h1>Pie</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
